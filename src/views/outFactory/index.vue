2e0c3a5311145218494ab16c11921b4764978421
大诚专用 <83280028+ChenCheng19981014@users.noreply.github.com>
2024-04-08 22:09:26 +0800

    前端无法解析带公式的excel表格

--- src/views/outFactory/index.vue
+++ src/views/outFactory/index.vue
@@ -1,7 +1,14 @@
 <style scoped lang="scss">
+@import "@/design/hooks.scss";
 .outFactory {
   width: 100%;
   height: 100%;
+  @include center;
+  .tips {
+    font-size: 50px;
+    font-family: "pm1";
+    color: #ffffff;
+  }
 }
 </style>
 
@@ -11,33 +18,26 @@ import * as XLSX from "xlsx";
 import axios from "axios";
 
 const readerExcel = async () => {
-    const response = await axios.get("./excel/AIOC.xlsx", {
-      responseType: "arraybuffer",
-    });
-//   const response = await axios.get("./excel/t.xls", {
-//     responseType: "arraybuffer",
-//   });
+  // const response = await axios.get("./excel/AIOC.xlsx", {
+  //   responseType: "arraybuffer",
+  // });
+  const response = await axios.get("./excel/t.xls", {
+    responseType: "arraybuffer",
+  });
   const data = new Uint8Array(response.data);
-  const workbook = XLSX.read(data, { type: "buffer" }); // 使用 'array' 类型解析
+  const workbook = XLSX.read(data, { type: "array" }); // 使用 'array' 类型解析
   const wsname = workbook.SheetNames[0]; //取第一张表，wb.SheetNames[0]是获取Sheets中第一个Sheet的名字
 
-  const ws = XLSX.utils.sheet_to_json(workbook.Sheets[wsname]); //生成json表格内容，wb.Sheets[Sheet名]获取第一个Sheet的数据
-
+  const ws = XLSX.utils.sheet_to_json(workbook.Sheets[wsname], {
+    header: 1,
+    raw: false,
+  }); //生成json表格内容，wb.Sheets[Sheet名]获取第一个Sheet的数据
   const excellist = [] as any; //清空接收数据
-
   //编辑数据
   for (var i = 0; i < ws.length; i++) {
     excellist.push(ws[i]);
   }
-
-  console.log(
-    // "第一个表名：",
-    // wsname,
-    "\n读取结果",
-    excellist
-    // "\n表信息：",
-    // sheet
-  );
+  console.log("第一个表名：", wsname, "\n读取结果", excellist);
 };
 
 onMounted(() => {
@@ -46,5 +46,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="outFactory"></div>
+  <div class="outFactory">
+    <div class="tips">测试文字...</div>
+  </div>
 </template>
