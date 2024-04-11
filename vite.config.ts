db209313f7ddd3a6f6f650eb60b2b6eb5198a049
大诚专用 <cc2524261183@163.com>
2024-04-11 13:57:07 +0800

    封装右侧组价

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import svgLoader from "vite-svg-loader";
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   //项目部署的基础路径
   base: "/",
   //静态资源服务的文件夹
