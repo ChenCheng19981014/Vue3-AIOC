aeeb711f20d01a0a7eed0d6aa59cd726b02e4b00
亦世凡华、 <2014106247@qq.com>
2024-04-22 16:11:40 +0800

    样式

--- src/components/global/line-chart-common.vue
+++ src/components/global/line-chart-common.vue
@@ -229,7 +229,7 @@ onMounted(() => {
     background: var(--color-basic-black-black-60, rgba(0, 0, 0, 0.60));
     color: var(--color-basic-cyan-cyan-1, #00FFE0);
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    font-family: D-DIN;
+    font-family: "ding";
     font-size: 14px;
     font-style: normal;
     font-weight: 700;
